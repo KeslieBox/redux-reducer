4132e260c01bb74de5fff83a4b36b63c116ea48f
KeslieBox <kesliebox@gmail.com>
2021-05-21 01:48:22 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -10,14 +10,16 @@ export function manageFriends(state = {friends: []}, action){
         case 'REMOVE_FRIEND':
             const removeI = state.friends.findIndex(f => f.id === action.id)
 
-            return (
-                {...state, 
-                    friends: [
-                        ...state.friends.slice(0, removeI), 
-                        ...state.friends.slice(removeI + 1),
-                    ]
-                }
-            )
+            // return (
+            //     {...state, 
+            //         friends: [
+            //             ...state.friends.slice(0, removeI), 
+            //             ...state.friends.slice(removeI + 1),
+            //         ]
+            //     }
+            // )
+            return Object.assign({}, state, {
+                friends: [...state.friends.slice(0, removeI), ...state.friends.slice(removeI + 1)]})
         default:
             return state
     }
