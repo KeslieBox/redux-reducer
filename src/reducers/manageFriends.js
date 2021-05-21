49d4d436b725818871f017f52b5027e08fa20e53
KeslieBox <kesliebox@gmail.com>
2021-05-21 01:44:26 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type){
+        case 'ADD_FRIEND': 
+            return Object.assign({}, state, {
+                friends: 
+                [...state.friends, action.friend]
+            })
+            // also: 
+            // return ({...state, friends: [...state.friends, action.friend]})
+        case 'REMOVE_FRIEND':
+            const removeI = state.friends.findIndex(f => f.id === action.id)
+
+            return (
+                {...state, 
+                    friends: [
+                        ...state.friends.slice(0, removeI), 
+                        ...state.friends.slice(removeI + 1),
+                    ]
+                }
+            )
+        default:
+            return state
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0,}, action){
+    switch(action.type){
+        case 'INCREASE':
+            // this...
+            // return Object.assign({}, state, {
+            //     numberOfPresents: state.numberOfPresents + 1
+            // })
+            // orrrr this: 
+            return {numberOfPresents: state.numberOfPresents + 1}
+        default: 
+            return state
+    }
 }
